b83cde47e02fc9e1d8232da501ebfeb35520b442
Tanveer <tanveer___@hotmail.com>
2019-06-03 22:08:55 +0200

    First commit with navigation bar

---
+++ Password/app/src/main/java/com/example/password/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.password;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private Fragment currentFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        Fragment firstFragment = new HomeFragment();
+        loadFragment(firstFragment);
+        currentFragment = firstFragment;
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null && fragment != currentFragment) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayout, fragment)
+                    .commit();
+            currentFragment = fragment;
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_safe:
+                fragment = new SafeFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+}
